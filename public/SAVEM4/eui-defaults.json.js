f1e57ac3864ec090c3e1a154a66265e7b2422edb
John Pywtorak <jpywtora@gmail.com>
2014-09-02 16:19:05 -0700

    Changed the begin.eui bootstrap process to let the VWF have the initialization back. In other words to not do anything there that could take time to complete like make an ajax call which causes hanging currently, especially if it fails. Now the config json is loaded via script tag, although the VWF is controlling it and loading it using jQuery and not the browser. so a tail ajax request still counts there. That means your <script> sources must not result in a 404. Installers should copy eui-defaults.json.js to eui.json.js and modify it as needed.

--- public/SAVEM4/eui-defaults.json.js
+++ public/SAVEM4/eui-defaults.json.js
@@ -1,3 +1,4 @@
+var __EUI =
 {
     "default": "This is the default EUI settings, copy, and save as eui.json for modified settings",
     "activityServerAddress": "http://localhost:3001/M4clear/action",
@@ -5,4 +6,4 @@
     "assessmentServerAddress": "http://localhost:3001/M4clear/assessment",
     "M4_Carbine": "myM4",
     "toolTrayObjects": [ "M4_Carbine" ]
-}
+};
