de5dedab9291df29e0718792a945796198de1f37
John Pywtorak <jpywtora@gmail.com>
2014-09-23 17:03:55 -0700

    Completed tool tray request and object instance requests. Reworked configuration and initialization of the view and model. Almost all is dynamic except the json for the m4.

--- public/SAVEM4/eui-defaults.json.js
+++ public/SAVEM4/eui-defaults.json.js
@@ -2,7 +2,5 @@
 
 var __EUI = {
     "default": "This is the default EUI settings, copy, and save as eui.json for modified settings",
-    "baseServerAddress": "http://localhost:3001/M4clear",
-    "M4_Carbine": "myM4",
-    "toolTrayObjects": [ "M4_Carbine" ]
+    "baseServerAddress": "http://localhost:3001/M4clear"
 };
