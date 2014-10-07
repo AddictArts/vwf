7ab27de8ab6eacad8a0256a3f26d78bc5aa5470f
John Pywtorak <jpywtora@gmail.com>
2014-10-07 15:17:13 -0700

    Changed the clear the weapon eui scenario to use the repository for the assets versus local files which will go away * Fixed texture references in M4.dae * Fixed rotations on the M4 where it was lying flat instead of upright * Removed commented example code

--- public/SAVEM4/echoandtestingtxrx.js
+++ public/SAVEM4/echoandtestingtxrx.js
@@ -97,7 +97,7 @@ routes.post(routes.OBJECT, function(req, res) {
     if (o.type == 'create') {
         data = {
             KbId: "myM4",
-            assetURL: "models/M4_v09.dae"
+            assetURL: "../../SAVE/models/weapons/M4/M4.dae"
         };
     }
 
