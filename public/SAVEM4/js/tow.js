b328eed47621b329d8180327ea1b3a2be877b2fb
John Pywtorak <jpywtora@gmail.com>
2014-10-10 15:09:57 -0700

    Added a little documentation to tow.js and improved the test scene. Moved the test scene to a test folder. Added the Blender shooting range.

--- public/SAVEM4/js/tow.js
+++ public/SAVEM4/js/tow.js
@@ -2,6 +2,51 @@
 // tow.js
 // dependencies: three.js https://github.com/mrdoob/three.js
 //               tween.js https://github.com/sole/tween.js (optional)
+
+/*
+EXAMPLE:
+
+TOW.loadCollada('xyz.dae', function() {
+  $xyz_dae.scale.set(0.005, 0.005, 0.005);
+  TOW.render();
+});
+
+TOW.loadColladas([ 'a.dae', 'b.dae' ], function() {
+  TOW.render(function(delta) {
+    $a..rotation.y += 0.005;
+    $b..rotation.y += Math.PI * delta;
+  });
+});
+
+TOW.loadCollada('a.dae', function() {
+  TOW.findMeshVisibleAndCenterRender('child', $a, function(delta, pivot) {
+  pivot.rotation.y += 0.005;
+}, false);
+
+TOW.intow(-1, 0.5, 0.5, -0.25, 0.025, 0.25, 0, 0, 0); // light, camera, target ... x,y,z
+
+TWEEN:
+var pivot =  TOW.findMeshAndVisibleMesh('child', $a);
+
+TOW.render(function(delta) { pivot.rotation.y += Math.PI * delta; });
+pivot.position.set(0, 0, 0);
+
+var curZ = { z:  pivot.rotation.z };
+var pull = new TWEEN.Tween(curZ)
+  .to({ z: 0.52 }, 1000) // radians and milliseconds
+  // .easing(TWEEN.Easing.Elastic.InOut)
+  .easing(TWEEN.Easing.Exponential.InOut)
+  .onUpdate(function() { curZ = pivot.rotation.z = -this.z; });
+var release = new TWEEN.Tween(curZ)
+  .to({ z: 0 }, 500)
+  .onUpdate(function() { curZ =  pivot.rotation.z = -this.z })
+  .delay(250);
+
+pull.chain(release);
+release.chain(pull);
+pull.start();
+*/
+
 var TOW = { REVISION: '0.1' };
 
 TOW.Fov = 40;
