d9de3ee3b4e20517bd21b06d6293068dbbaf8fde
John Pywtorak <jpywtora@gmail.com>
2014-12-05 14:41:24 -0800

    Added a default asset string for exercise saving of assets that don't have behavior and checks for double quotes in the templates

--- scripts/jsstringit.js
+++ scripts/jsstringit.js
@@ -12,7 +12,21 @@ var path = require('path'),
 function createPublishedFiles(basePath) {
     var ihtml, // index.vwf.html
         iyaml, // index.vwf.yaml
-        byaml; // M4_Carbine_dae.eui.yaml
+        byaml, // M4_Carbine_dae.eui.yaml
+        defaultAssetEuiByaml = "\
+---\n\
+properties:\n\
+  actionNames: [ 'Attach', 'Close', 'Detach', 'Extract', 'Insert', 'Inspect', 'Lift', 'Open', 'Point', 'Press', 'Pull', 'PullAndHold', 'Push', 'PushAndHold', 'Release' ]\n\
+methods:\n\
+  setup:\n\
+scripts:\n\
+- |\n\
+  this.setup = function() {\n\
+    console.info(this.id + ' ' + this.name + ' setup');\n\
+    //#!=asset-translation;\n\
+    //#!=asset-rotation;\n\
+  };\n\
+";
 
     puts('// Copyright 2014, SRI International');
     puts('// jsstringit Creating index_vwf_html, index_vwf_yaml, and M4_Carbine_dae_eui_yaml from ' + basePath);
@@ -23,12 +37,19 @@ function createPublishedFiles(basePath) {
         byaml = fs.readFileSync(basePath + '/M4_Carbine_dae.eui.yaml');
     } catch (e) { console.log(e); }
 
+    if (ihtml.toString().indexOf('"') != -1) console.error('Quote found in template index.vwf.html, change to single quote');
+    if (iyaml.toString().indexOf('"') != -1) console.error('Quote found in template index.vwf.yaml, change to single quote');
+    if (byaml.toString().indexOf('"') != -1) console.error('Quote found in template M4_Carbine_dae.eui.yaml, change to single quote');
+    if (defaultAssetEuiByaml.indexOf('"') != -1) console.error('Quote found in template defaultAssetEuiByaml, change to single quote');
+
     puts('// Copyright 2014, SRI International\n//1\n');
     puts('var index_vwf_html = "\\' + ihtml.toString().split('\n').join('\\n\\\n') + '";');
     puts('\n//2\n');
     puts('var index_vwf_yaml = "\\' + iyaml.toString().split('\n').join('\\n\\\n') + '";');
     puts('\n//3\n');
     puts('var M4_Carbine_dae_eui_yaml = "\\' + byaml.toString().split('\n').join('\\n\\\n') + '";');
+    puts('\n//4\n');
+    puts('var default_Asset_Eui_B_yaml = "\\' + defaultAssetEuiByaml.split('\n').join('\\n\\\n') + '";');
 }
 
 if (process.argv.length > 2) {
