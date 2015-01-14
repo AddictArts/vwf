851add194d2d99a8b4e4aa085fa49d509dce6678
unknown <cgreuel@cgreuel-e7440-lt.esg.sri.com>
2015-01-14 15:50:47 -0800

    Small fix to URL, nix "repos".

--- public/published/clearing/eui.json.js
+++ public/published/clearing/eui.json.js
@@ -1,5 +1,5 @@
 // Copyright 2014, SRI International
 
 var __EUI = {
-    "baseServerAddress": "http://semantic3d.com:3001/repos/exercises/071-100-0032/step01/m4_flora_clear"
+    "baseServerAddress": "http://semantic3d.com:3001/exercises/071-100-0032/step01/m4_flora_clear"
 };
