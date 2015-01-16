0d2e0f0c9e27d07528c19b1441f952409a3e1c15
John Pywtorak <jpywtora@gmail.com>
2015-01-15 23:00:25 -0800

    Fixed grouping to jsTree and test html

--- public/js/s3d.js
+++ public/js/s3d.js
@@ -8,10 +8,10 @@ var G2JS = require('grouping2js'),
 var updateModelTree = function(treeList) {
   $('#tree').jstree({
     core : {
-        data : [ ],
+        data : treeList,
         check_callback: true
     },
-    plugins : [ 'state', 'contextmenu' ],
+    plugins : [ 'state', 'contextmenu' ], //XXX consider not using state...
     contextmenu : {
         items : { },
         ccp : false,
@@ -22,68 +22,35 @@ var updateModelTree = function(treeList) {
   });
 };
 
-// [ { "id" : "M4 Carbine",          "parent" : "#",                    "text" : "M4 Carbine" },
-// { "id" : "Sling",                 "parent" : "M4 Carbine",           "text" : "Sling" },
-// { "id" : "Barrel_Assembly",       "parent" : "M4 Carbine",           "text" : "Barrel_Assembly" },
-// { "id" : "Buttstock Group",       "parent" : "M4 Carbine",           "text" : "Buttstock Group" },
-// { "id" : "Buttstock",             "parent" : "Buttstock Group",      "text" : "Buttstock" },
-// { "id" : "Swivel_LAMA1259863095", "parent" : "Buttstock Group",      "text" : "Swivel_LAMA1259863095k" },
-// { "id" : "Magazine_g Group",      "parent" : "M4 Carbine",           "text" : "Magazine_g Group" },
-// { "id" : "Tube",                  "parent" : "Magazine_g Group",     "text" : "Tube" },
-// { "id" : "Lower_Receiver Group",  "parent" : "M4 Carbine",           "text" : "Lower_Receiver Group" },
-// { "id" : "Lower_Receiver",        "parent" : "Lower_Receiver Group", "text" : "Lower_Receiver" },
-// { "id" : "Upper_Receiver Group",  "parent" : "M4 Carbine",           "text" : "Upper_Receiver Group" },
-// { "id" : "Upper_Receiver",        "parent" : "Upper_Receiver Group", "text" : "Plunger_Assembly" } ]
+var transformGroupingTojsTree = function(groupingObj, parent, treeList) {
+    var treeList = treeList || [ ],
+        parent = parent || groupingObj.name;
 
-//  {
-//     name: "ShootingRange",
-//     groups: [
-//         {
-//             name: "environment",
-//             node: "environment",
-//             parts: [
-//                 "grass",
-//                 "tree_line",
-//                 "sky",
-//                 "targets",
-//                 "ShootingRangeArea1",
-//                 "ShootingRangeArea2",
-//                 "ShootingRangeArea3",
-//                 "ShootingRangeArea4",
-//                 "ShootingRangeArea5",
-//                 "ShootingRangeArea6",
-//                 "ShootingRangeArea7",
-//                 "ShootingRangeArea8"
-//             ]
-//         }
-//     ]
-// }
-var transformGroupingTojsTree = function(groupingObj, treeList) {
-    var treeList = treeList || [ ];
-
-    treeList.push({
-        id: groupingObj.name,
-        parent: '#',
-        text: groupingObj.name
-    });
-
-    return treeList; //XXX
+    if (treeList.length == 0) {
+        treeList.push({
+            id: groupingObj.name,
+            parent: '#',
+            text: groupingObj.name
+        });
+    }
 
     for (var p in groupingObj) {
         if (p == 'groups') {
             groupingObj.groups.forEach(function(group) {
-                var children = treeList;
-
-                if (group.node) {
-                    treeList.children[ group.node ] = { extends: "http://vwf.example.com/node3.vwf", children: { } };
-                    children = treeList.children[ group.node ];
-                }
-
-                transformGroupingTojsTree(group, children);
+                treeList.push({
+                    id: group.name,
+                    parent: parent,
+                    text: group.name
+                });
+                transformGroupingTojsTree(group, group.name, treeList);
             });
         } else if (p == 'parts') {
             groupingObj.parts.forEach(function(part) {
-                treeList.children[ part ] = { extends: "http://vwf.example.com/node3.vwf" };
+                treeList.push({
+                    id: part,
+                    parent: parent,
+                    text: part
+                });
             });
         }
     }
@@ -103,19 +70,13 @@ var loadS3D = function(url) {
         if ($.isXMLDoc(data)) xmlString = (new window.XMLSerializer()).serializeToString( data );
         else xmlString = data;
 
-        console.log(xmlString);
+        // console.log(xmlString);
 
         var grouping = G2JS.g2js(xmlString),
             treeList = transformGroupingTojsTree(grouping);
 
-        console.log(grouping);
-        console.log(treeList);
-        console.log(JSON.stringify(treeList));
         updateModelTree(treeList);
-
-        var o = G2JS.g2html(xmlString);
-
-        console.log(o.text);
+        // console.log(G2JS.g2html(xmlString).text);
     })
     .fail(function(jqXHR, textStatus, errorThrown) {
       console.warn(textStatus + ':' + errorThrown);
@@ -146,13 +107,8 @@ var getListOfDAE = function(url) {
     });
 };
 
-
 window.$ = $;
 window.jQuery = $;
-// getListOfS3D();
-
-// document.addEventListener("DOMContentLoaded", function(event) {
-// window.addEventListener("load", function(event) {
 window.addEventListener("DOMContentLoaded", function(event) {
     console.log("DOM fully loaded and parsed");
     getListOfS3D();
