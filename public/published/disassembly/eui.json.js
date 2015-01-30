80a9f47dd0b5047639ce56acc5d6f343a3b1cdf1
John Pywtorak <jpywtora@gmail.com>
2015-01-29 17:07:28 -0800

    Added a published disassembly example which has the initial state set

---
+++ public/published/disassembly/eui.json.js
@@ -0,0 +1,7 @@
+// Copyright 2014, SRI International
+
+var __EUI = {
+    // "baseServerAddress": "http://localhost:3001/published/disassembly"
+    // "baseServerAddress": "http://semantic3d.com:3001/exercises/071-100-0032/step01/m4_flora_dis"
+    "baseServerAddress": "http://localhost:3001/exercises/071-100-0032/step01/m4_flora_dis"
+};
